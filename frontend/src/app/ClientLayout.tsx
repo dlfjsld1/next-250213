4931ac833c34853cda6f2c222dab017b3077f031
dlfjsld1 <ltk900216@gmail.com>
2025-02-14 15:37:32 +0900

    logined check

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import { components } from "@/lib/backend/apiV1/schema";
 import client from "@/lib/backend/client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function ClientLayout({
+    me,
     children,
   }: Readonly<{
+    me: components["schemas"]["MemberDto"];
     children: React.ReactNode;
   }>) {
-    const router = useRouter()
+    const router = useRouter();
+    const isLogined = me.id !== 0;
 
     return (<html lang="en">
         <body className="min-h-[100dvh] flex flex-col">
@@ -17,7 +21,7 @@ export default function ClientLayout({
             <Link href="/">메인</Link>
             <Link href="/about">소개</Link>
             <Link href="/post/list">글 목록</Link>
-            <Link href="/member/login">로그인</Link>
+            {!isLogined ? <Link href="/member/login">로그인</Link> :
             <Link href="" 
                 onClick={async e => {
                     e.preventDefault();
@@ -30,8 +34,8 @@ export default function ClientLayout({
                         return;
                     }
                     router.push("/post/list")
-            }}>로그아웃</Link>
-            <Link href="/member/me">내정보</Link>
+            }}>로그아웃</Link>}
+            {isLogined && <Link href="/member/me">내정보</Link>}
           </header>
           <div className="flex-grow">{children}</div>
           <footer>푸터</footer>
