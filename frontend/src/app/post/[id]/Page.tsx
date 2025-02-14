1c5e9c07e1544565e601a665741755979abcd783
dlfjsld1 <ltk900216@gmail.com>
2025-02-14 10:52:12 +0900

    detail

--- frontend/src/app/post/[id]/Page.tsx
+++ frontend/src/app/post/[id]/Page.tsx
@@ -1,1 +1,32 @@
-export default async function Page() {}
+import createClient from "openapi-fetch";
+import ClientPage from "./ClientPage";
+import { paths } from "@/lib/backend/apiV1/schema";
+
+
+const client = createClient<paths>({
+    baseUrl: "http://localhost:8080",
+});
+
+export default async function Page({
+    //params는 주소에서 들어오는 매개변수 객체
+    params
+} : {
+    params: {
+        id: number;
+    };
+}) {
+    const { id } = await params;
+
+    const response = await client.GET("/api/v1/posts/{id}", {
+        params: {
+            path: {
+                id,
+            }
+        }
+    });
+
+    const rsData = response.data!!; // 여기는 null이나 undefined가 절대 들어오지 않는다.
+    const post = rsData.data;
+    
+    return <ClientPage post={post} />;
+}
