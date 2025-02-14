8ba00570d84c39162892763fd7722796f2c1429d
dlfjsld1 <ltk900216@gmail.com>
2025-02-14 14:49:53 +0900

    my info server fetch

--- frontend/src/app/member/me/ClientPage.tsx
+++ frontend/src/app/member/me/ClientPage.tsx
@@ -4,34 +4,11 @@ import { components } from "@/lib/backend/apiV1/schema";
 import client from "@/lib/backend/client";
 import { useEffect, useState } from "react";
 
-export default function ClientPage() {
-
-  const [me, setMe] = useState<components["schemas"]["MemberDto"] | null>(null);
-
-  const getMe = async () => {
-    const response = await client.GET("/api/v1/members/me", {
-      credentials: "include",
-    })
-    
-    if (response.error) {
-      alert(response.error.msg);
-      return;
-    }
-    
-    const rsData = response.data;
-    const memberDto = rsData.data;
-    
-    setMe(memberDto);
-  }
-
-
-  useEffect(() => {
-    getMe();
-  }, [])
-
-  if(me === null) {
-    return <div>로딩중..</div>;
-  }
+export default function ClientPage({
+  me
+}: {
+  me: components["schemas"]["MemberDto"];
+}) {
 
   return (
     <>
--- frontend/src/app/member/me/page.tsx
+++ frontend/src/app/member/me/page.tsx
@@ -1,8 +1,22 @@
 import createClient from "openapi-fetch";
 import ClientPage from "./ClientPage";
 import { paths } from "@/lib/backend/apiV1/schema";
+import client from "@/lib/backend/client";
+import { cookies } from "next/headers";
 
 export default async function Page() {
 
-  return <ClientPage />;
+  const response = await client.GET("/api/v1/members/me", {
+    headers: {
+      cookie: (await cookies()).toString(),
+    }
+  })
+
+  if (response.error) {
+    return <div>{response.error.msg}</div>;
+  }
+
+  const memberDto = response.data.data;
+
+  return <ClientPage me={memberDto} />;
 }
